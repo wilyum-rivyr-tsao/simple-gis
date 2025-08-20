74d36f46459bd8ddd4bad4e13d85afa83d4221fb
liam <vitalitymailg@gmail.com>
2025-08-20 10:05:25 +0800

    feat: integrate Tailwind CSS and remove legacy styles
    - Added PostCSS configuration for Tailwind CSS and Autoprefixer.
    - Replaced global CSS styles with Tailwind's base, components, and utilities.
    - Removed unused page.module.css file.
    - Updated page.tsx to use Tailwind CSS classes for styling the source link.
    - Created Tailwind configuration file to specify content paths.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <main>
       <a
-        className={styles['source-link']}
+        className="fixed z-10 p-4 text-white bg-black rounded-lg cursor-pointer top-1 left-1"
         href='https://github.com/hyundotio/nextjs-ts-cesium-example'
         target='_blank'
         rel='noreferrer noopener'
